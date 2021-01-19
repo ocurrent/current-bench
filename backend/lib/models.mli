ad0dc3336d68d5d7ecd7375abe226e1d8ebd14b6
Rizo I <rizo@odis.io>
2021-01-19 17:38:28 +0000

    Fix missing Benchmark module in interface file.

--- backend/lib/models.mli
+++ backend/lib/models.mli
@@ -1,34 +1,35 @@
-type t
+module Benchmark : sig
+  type t
 
-val make :
-  run_at:Ptime.t ->
-  duration:Ptime.span ->
-  repo_id:string * string ->
-  commit:string ->
-  ?branch:string ->
-  ?pull_number:int ->
-  ?name:string ->
-  Yojson.Safe.t ->
-  t
+  val make :
+    run_at:Ptime.t ->
+    duration:Ptime.span ->
+    repo_id:string * string ->
+    commit:string ->
+    ?branch:string ->
+    ?pull_number:int ->
+    Yojson.Safe.t ->
+    t
 
-val run_at : t -> Ptime.t
+  val run_at : t -> Ptime.t
 
-val duration : t -> Ptime.span
+  val duration : t -> Ptime.span
 
-val repo_id : t -> string * string
+  val repo_id : t -> string * string
 
-val commit : t -> string
+  val commit : t -> string
 
-val branch : t -> string option
+  val branch : t -> string option
 
-val pull_number : t -> int option
+  val pull_number : t -> int option
 
-val name : t -> string option
+  val name : t -> string option
 
-val data : t -> Yojson.Safe.t
+  val data : t -> Yojson.Safe.t
 
-val pp : t Fmt.t
+  val pp : t Fmt.t
 
-module Db : sig
-  val insert : Postgresql.connection -> t -> unit
+  module Db : sig
+    val insert : Postgresql.connection -> t -> unit
+  end
 end
