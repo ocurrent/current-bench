36646e9cc55658f2195f0a151a508419f2c24ff8
Rizo I <rizo@odis.io>
2021-01-19 16:30:22 +0000

    Add Models module with a benchmark model.

---
+++ backend/lib/models.mli
@@ -0,0 +1,34 @@
+type t
+
+val make :
+  run_at:Ptime.t ->
+  duration:Ptime.span ->
+  repo_id:string * string ->
+  commit:string ->
+  ?branch:string ->
+  ?pull_number:int ->
+  ?name:string ->
+  Yojson.Safe.t ->
+  t
+
+val run_at : t -> Ptime.t
+
+val duration : t -> Ptime.span
+
+val repo_id : t -> string * string
+
+val commit : t -> string
+
+val branch : t -> string option
+
+val pull_number : t -> int option
+
+val name : t -> string option
+
+val data : t -> Yojson.Safe.t
+
+val pp : t Fmt.t
+
+module Db : sig
+  val insert : Postgresql.connection -> t -> unit
+end
