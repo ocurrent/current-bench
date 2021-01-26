90c817cdb04dd962556e4192355c375ed50c37d4
Rizo I <rizo@odis.io>
2021-01-26 12:58:05 +0000

    Benchmarks table migration (#25)
    * Do not create benchmars table on pg init.
    
    * Remove duration benchmarks table schema.
    
    I think it's best to have `duration` (or `time`) under metrics.
    
    * Add benchmarksrun to benchmarks migration sql script.

--- backend/db/migrations/20210013150054_add_benchmarks_table.up.sql
+++ backend/db/migrations/20210013150054_add_benchmarks_table.up.sql
@@ -1,10 +1,9 @@
-CREATE TABLE benchmarks(
-    run_at timestamp without time zone not null,
-	duration interval NOT NULL,
-	repo_id varchar(256) NOT NULL,
-	commit varchar(50) NOT NULL,
-	branch varchar(256),
-	pull_number integer,
-	name varchar(256),
-	data jsonb
-);
+	CREATE TABLE benchmarks(
+		run_at timestamp without time zone not null,
+		repo_id varchar(256) NOT NULL,
+		commit varchar(50) NOT NULL,
+		branch varchar(256),
+		pull_number integer,
+		name varchar(256),
+		metrics jsonb
+	);
