4a222551aefb311b08f893df325bab09a9eae4d9
Rizo I <rizo@odis.io>
2021-01-26 12:49:10 +0000

    Do not create benchmars table on pg init.

--- backend/postgres/init.sql
+++ backend/postgres/init.sql
@@ -1,13 +1,5 @@
 GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
 
-CREATE TABLE benchmarks(
-	repositories varchar(256),
-	commits varchar(50) NOT NULL,
-	json_data jsonb,
-	timestamp float8,
-	branch varchar(256)
-);
-
 CREATE TABLE benchmarksrun (
     commits varchar(50),
     name varchar(100),
