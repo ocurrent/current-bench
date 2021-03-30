b390f5013851628edca74d5bd081b1810f524708
Rizo I <rizo@odis.io>
2021-03-30 12:18:51 +0100

    Add current_util.mli.

---
+++ pipeline/lib/current_util.mli
@@ -0,0 +1,13 @@
+val get_job_id : 'a Current.t -> string option Current.t
+
+module Docker_util : sig
+  val pread_log :
+    ?label:string ->
+    ?pool:unit Current.Pool.t ->
+    ?run_args:string list ->
+    Current_docker.Default.Image.t Current.t ->
+    args:string list ->
+    string Current.t
+  (** Similar to {!val:Current_docker.Default.pred} but includes the output in
+      the job's logs. *)
+end
