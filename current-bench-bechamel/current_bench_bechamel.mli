37d2034311f071e315df4f3d8e7d2a946ed76d57
Rizo I <rizo@odis.io>
2021-04-27 14:28:36 +0100

    Add current-bench-bechamel (#109)
    * Add _build to gitignore.
    
    * Add current-bench-bechamel library.

---
+++ current-bench-bechamel/current_bench_bechamel.mli
@@ -0,0 +1,9 @@
+type 'a result = (string, 'a) Hashtbl.t
+
+type 'a results = (string, 'a result) Hashtbl.t
+
+val json_of_ols_results :
+  ?name:string -> Bechamel.Analyze.OLS.t results -> Yojson.Safe.t
+(** [json_of_ols_results ?name ols] is a JSON value containing a list of test
+    metrics encoded from the OLS results. [name] is the name of the benchmark,
+    will be omitted by default. *)
