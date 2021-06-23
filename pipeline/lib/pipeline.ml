f10fe278cfeee49d6438235d92aa57e8e23075f5
Rizo I <rizo@odis.io>
2021-06-23 19:43:29 +0100

    Refactor Pipeline.monitor_commit and make sure storage/logging works.

--- pipeline/lib/pipeline.ml
+++ pipeline/lib/pipeline.ml
@@ -28,53 +28,83 @@ let log_commit_info job commit_context =
     Fmt.Dump.(option string)
     branch commit
 
-let log_job_output job output = Current.Job.log job "Output: %S" output
-
 let string_of_output output =
   String.concat "\n" (List.map Yojson.Safe.pretty_to_string output)
 
-let monitor_commit ~(config : Config.t) commit_context =
-  let* commit_context = commit_context in
-  let output =
-    let conninfo = Uri.to_string config.db_uri in
-    Postgresql_util.with_connection ~conninfo (fun db ->
-        let commit = Commit_context.fetch commit_context in
-        let current_build =
-          Engine.Docker_engine.build ~pool commit_context commit
-        in
-        let* build_job_id =
-          Current_util.get_job_id current_build |> Current.map Option.get
-        in
-        let repo_id_string = Commit_context.repo_id_string commit_context in
-        let () =
-          let branch = Commit_context.branch commit_context in
-          let pull_number = Commit_context.pull_number commit_context in
-          let commit = Commit_context.hash commit_context in
-          Storage.record_build_start ~repo_id:repo_id_string ~pull_number
-            ~commit ~branch ~build_job_id db
-        in
-        let* state = current_build in
-        let current_run = Engine.Docker_engine.run ~config current_build in
-        let* run_job_id =
-          Current_util.get_job_id current_run |> Current.map Option.get
-        in
-        let run_job = Current.Job.lookup_running run_job_id |> Option.get in
-        log_commit_info run_job commit_context;
-        let* output = current_run in
-        let output_string = string_of_output output in
-        log_job_output run_job output_string;
-        let* () =
-          Engine.Docker_engine.complete commit_context state current_run
-        in
-        Storage.record_run_finish ~repo_id_string ~build_job_id ~output db;
-        current_run)
-  in
-  let* () =
-    Reporting.Slack.post ~path:config.slack_path
-      (Current.map string_of_output output)
-  in
-  let* () = Reporting.Github.post commit_context output in
-  Current.return ()
+let build_commit ~db commit_context =
+  Current.component "build"
+  |> let** commit_context = commit_context in
+     let commit = Commit_context.fetch commit_context in
+     let img = Engine.Docker_engine.build ~pool commit_context commit in
+     let current_job_id_opt = Current_util.get_job_id img in
+     let* job_id_opt = current_job_id_opt in
+     match job_id_opt with
+     | None ->
+         Fmt.epr "build job id not ready@.";
+         Current.pair img current_job_id_opt
+     | Some build_job_id ->
+         let () =
+           match Current.Job.lookup_running build_job_id with
+           | None -> Fmt.epr "build job %s is not running@." build_job_id
+           | Some job -> log_commit_info job commit_context
+         in
+         let repo_id_string = Commit_context.repo_id_string commit_context in
+         let branch = Commit_context.branch commit_context in
+         let pull_number = Commit_context.pull_number commit_context in
+         let commit = Commit_context.hash commit_context in
+         Storage.record_build_start ~repo_id:repo_id_string ~pull_number ~commit
+           ~branch ~build_job_id db;
+         Current.pair img current_job_id_opt
+
+let run_commit ~db ~config ~commit_context ~build_job_id img =
+  let info = Commit_context.show commit_context in
+  let output = Engine.Docker_engine.run ~info ~pool ~config img in
+  let* run_job_id_opt = Current_util.get_job_id output in
+  match run_job_id_opt with
+  | None ->
+      Fmt.epr "run job id not ready@.";
+      Current.return None
+  | Some run_job_id ->
+      let repo_id_string = Commit_context.repo_id_string commit_context in
+      let* output = Current.map Json_util.parse_many output in
+      Storage.record_run_finish ~repo_id_string ~build_job_id ~run_job_id
+        ~output db;
+      Current.return (Some output)
+
+let monitor_commit ~(config : Config.t) current_commit_context =
+  Current.component "monitor_commit"
+  |> let** commit_context = current_commit_context in
+     Logs.debug (fun log -> log "monitor_commit");
+
+     let db =
+       let conninfo = Uri.to_string config.db_uri in
+       new Postgresql.connection ~conninfo ()
+     in
+
+     let build_state = build_commit ~db current_commit_context in
+     let img = Current.map fst build_state in
+     let* build_job_id_opt = Current.map snd build_state in
+
+     let* output =
+       match build_job_id_opt with
+       | None ->
+           Fmt.epr "build job id not ready@.";
+           Current.return None
+       | Some build_job_id ->
+           run_commit ~db ~config ~commit_context ~build_job_id img
+     in
+
+     match output with
+     | None -> Current.return ()
+     | Some output ->
+         let* () =
+           Reporting.Slack.post ~path:config.slack_path
+             (Current.return (string_of_output output))
+         in
+         let* () =
+           Reporting.Github.post current_commit_context (Current.return output)
+         in
+         Current.return ()
 
 module Github_pipeline = struct
   let github_api_of_oauth_file token =
