637e12a47f854ba27af8fa1c8d7d29c0db18945b
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-10-20 12:20:43 +0530

    Add a script to be able to run docker-compose command easily
    The project setup uses files in the environment directory, which are
    non-standard, and running docker-compose commands requires passing 3 arguments
    to get the command to run correctly.  This commit adds a script which makes it
    easy to run docker-compose commands.
    
    This would also make it easy to run variations of the start-development and
    stop-development commands.
    
    The script can be used to run commands, like
    
       ./scripts/dc.sh down -v
       ./scripts/dc.sh exec db psql -U docker

---
+++ scripts/dc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Helper script to run docker-compose commands easily
+
+# Ensure script is always run from the root of the repository
+cd $(dirname $0)/..
+
+docker-compose --project-name="current-bench" \
+               --file=./environments/development.docker-compose.yaml \
+               --env-file=./environments/development.env \
+               "$@"
