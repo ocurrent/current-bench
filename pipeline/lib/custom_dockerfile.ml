f6328ae5586d003fb507ed53a321278dd9ef06e3
Gargi Sharma <gs051095@gmail.com>
2021-10-21 18:38:37 +0200

    Look for *-bench.opam file in the repository
    We are already looking for a dockerfile in the filesystem,
    we use this to ensure that there's a opam file of the format
    *-bench.opam which we can use to then find dependencies. It
    means that there are lesser dependencies that we have to fetch.

--- pipeline/lib/custom_dockerfile.ml
+++ pipeline/lib/custom_dockerfile.ml
@@ -1,20 +1,16 @@
 open Current.Syntax
 module Docker = Current_docker.Default
 
-module File_exists = struct
+module Get_files = struct
   type t = unit
 
-  let id = "git-file-exists"
+  let id = "get-files"
 
   module Key = struct
-    type t = { commit : Current_git.Commit.t; filename : Fpath.t }
+    type t = { commit : Current_git.Commit.t }
 
     let to_json t =
-      `Assoc
-        [
-          ("commit", `String (Current_git.Commit.hash t.commit));
-          ("filename", `String (Fpath.to_string t.filename));
-        ]
+      `Assoc [ ("commit", `String (Current_git.Commit.hash t.commit)) ]
 
     let digest t = Yojson.Safe.to_string (to_json t)
 
@@ -22,37 +18,37 @@ module File_exists = struct
   end
 
   module Value = struct
-    type t = bool
+    type t = string list
 
-    let marshal t = if t then "true" else "false"
+    (* convert string list to string *)
+    let marshal t = String.concat "; " t
 
-    let unmarshal = function
-      | "true" -> true
-      | "false" -> false
-      | _ -> invalid_arg "not a boolean"
+    (* convert string to string list *)
+    let unmarshal t = String.split_on_char ';' t
   end
 
   open Lwt.Infix
 
-  let build () job { Key.commit; filename = target } =
+  let build () job { Key.commit } =
     Current.Job.start job ~level:Current.Level.Average >>= fun () ->
     Current_git.with_checkout ~job commit @@ fun dir ->
-    let filename = Fpath.(to_string (dir // target)) in
-    Lwt_unix.file_exists filename >>= fun ok -> Lwt.return (Ok ok)
+    let directory = Fpath.(to_string dir) in
+    Lwt_stream.to_list (Lwt_unix.files_of_directory directory) >>= fun ok ->
+    Lwt.return (Ok ok)
 
   let pp f key = Fmt.pf f "@[<v2>git file exists %a@]" Key.pp key
 
   let auto_cancel = true
 end
 
-module Cache = Current_cache.Make (File_exists)
+module Cache = Current_cache.Make (Get_files)
 
 let pp_commit = Fmt.(string)
 
-let file_exists ~repository filename =
-  Current.component "file-exists %S" (Fpath.to_string filename)
+let get_all_files ~repository =
+  Current.component "get-files"
   |> let> commit = Repository.src repository in
-     Cache.get () { File_exists.Key.commit; filename }
+     Cache.get () { Get_files.Key.commit }
 
 let base_dockerfile ~base =
   let open Dockerfile in
@@ -73,21 +69,17 @@ let minimal_dockerfile ~base =
   let open Dockerfile in
   base_dockerfile ~base @@ add_workdir
 
-let docker_exec ~pool ~run_args ~repository ~dockerfile args =
+let docker_exec ~label ~pool ~run_args ~repository ~dockerfile args =
   let { Repository.branch; pull_number; _ } = repository in
   let repo_info = Repository.info repository in
   let src = Repository.src repository in
   let image = Docker.build ~pool ~pull:false ~dockerfile (`Git src) in
   let commit = Repository.commit_hash repository in
-  Current_util.Docker_util.pread_log ~pool ~run_args ~repo_info ?pull_number
-    ?branch ~commit ~args image
+  Current_util.Docker_util.pread_log ~label ~pool ~run_args ~repo_info
+    ?pull_number ?branch ~commit ~args image
 
-let opam_install ~repository =
-  let repository_name = Repository.name repository in
-  (* FIXME: This should be supported by a custom Dockerfiles. *)
-  if String.equal repository_name "dune" then
-    "opam install ./dune-bench.opam -y --deps-only"
-  else "opam install -y --deps-only ."
+let opam_install ~opam_file =
+  Format.sprintf "opam install ./%s -y --deps-only ." opam_file
 
 let weekly = Current_cache.Schedule.v ~valid_for:(Duration.of_day 7) ()
 
@@ -95,17 +87,20 @@ let base = Docker.pull ~schedule:weekly "ocaml/opam"
 
 let with_base f = Current.map (fun base -> `Contents (f ~base)) base
 
-let discover_dependencies ~pool ~run_args ~repository =
+let discover_dependencies ~pool ~run_args ~repository ~opam_file =
   let cmd =
-    opam_install ~repository
+    opam_install ~opam_file
     ^ " --dry-run | sed -n '/^Installing \\(.*\\).$/{s//\\1/g;p}'"
   in
   let args = [ "/bin/sh"; "-c"; cmd ] in
   let dockerfile = with_base minimal_dockerfile in
-  let+ output = docker_exec ~pool ~run_args ~repository ~dockerfile args in
+  let+ output =
+    docker_exec ~label:"discover-dependencies" ~pool ~run_args ~repository
+      ~dockerfile args
+  in
   String.concat " " (String.split_on_char '\n' output)
 
-let dockerfile ~repository ~base ~dependencies =
+let dockerfile ~base ~dependencies ~opam_file =
   let open Dockerfile in
   let install_static_dependencies =
     if dependencies = "" then empty else run "opam install -y %s" dependencies
@@ -113,14 +108,28 @@ let dockerfile ~repository ~base ~dependencies =
   base_dockerfile ~base
   @@ install_static_dependencies
   @@ add_workdir
-  @@ run "%s" (opam_install ~repository)
+  @@ run "%s" (opam_install ~opam_file)
 
-let dockerfile ~pool ~run_args ~repository =
-  let* dependencies = discover_dependencies ~pool ~run_args ~repository in
-  with_base (dockerfile ~dependencies ~repository)
+let dockerfile ~pool ~run_args ~repository ~opam_file =
+  let* dependencies =
+    discover_dependencies ~pool ~run_args ~repository ~opam_file
+  in
+  with_base (dockerfile ~dependencies ~opam_file)
 
 let dockerfile ~pool ~run_args ~repository =
   let custom_dockerfile = Fpath.v "bench.Dockerfile" in
-  let* existing = file_exists ~repository custom_dockerfile in
-  if existing then Current.return (`File custom_dockerfile)
-  else dockerfile ~pool ~run_args ~repository
+  let* file_list = get_all_files ~repository in
+  Logs.info (fun logs ->
+      logs "These are all the fetched files %s" (String.concat "\n" file_list));
+  let dockerfile_exists =
+    List.mem (Fpath.to_string custom_dockerfile) file_list
+  in
+  let r = Str.regexp "^.*-bench\\.opam$" in
+  let acc = "" in
+  let opam_file =
+    List.fold_left
+      (fun v acc -> if Str.string_match r v 0 then acc ^ v else "")
+      acc file_list
+  in
+  if dockerfile_exists then Current.return (`File custom_dockerfile)
+  else dockerfile ~pool ~run_args ~repository ~opam_file
