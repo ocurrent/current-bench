57a4733e2284ae421b3c0389a6575641262eb086
Gargi Sharma <gs051095@gmail.com>
2021-11-17 12:12:08 +0100

    Skip old commits
    We currently re-run on all open PRs when we re-deploy CB. This means that
    sometimes we take more than a day to re-run all benchmarks (a lot of the
    older ones will fail because they don't have the changes to run the benchmarks).
    This patch skips all commits that are more than two weeks old.

--- pipeline/lib/util.ml
+++ pipeline/lib/util.ml
@@ -6,3 +6,14 @@ let get_branch_name branch =
   let prefix = "refs/heads/" in
   let len_prefix = String.length prefix in
   String.sub branch len_prefix (String.length branch - len_prefix)
+
+let stale_timestamp () =
+  let two_weeks_in_seconds = 1209600. in
+  let curr_gmt = Unix.gmtime (Unix.time () -. two_weeks_in_seconds) in
+  let year = curr_gmt.tm_year + 1900 in
+  let month = curr_gmt.tm_mon + 1 in
+  let day = curr_gmt.tm_mday in
+  let hour = curr_gmt.tm_hour in
+  let min = curr_gmt.tm_min in
+  let sec = curr_gmt.tm_sec in
+  Format.sprintf "%04d-%02d-%02dT%02d:%02d:%02dZ" year month day hour min sec
--- pipeline/lib/util.mli
+++ pipeline/lib/util.mli
@@ -1,3 +1,5 @@
 val read_fpath : Fpath.t -> string
 
 val get_branch_name : string -> string
+
+val stale_timestamp : unit -> string
