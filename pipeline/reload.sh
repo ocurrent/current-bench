82c4ee168ccca9cf68a0503488bf372fef14aec1
Arthur Wendling <art.wendling@gmail.com>
2021-11-24 13:27:11 +0100

    Hot reload pipeline on code changes for quick development

---
+++ pipeline/reload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+COMMAND="${*@Q}"
+
+function run {
+  cd /app
+  sh -c "$COMMAND" &
+}
+
+run
+
+while :
+do
+  cd /mnt/project
+
+  inotifywait -q -q -e CLOSE_WRITE \
+    lib/dune lib/*.ml lib/*.mli \
+    bin/dune bin/*.ml bin/*.mli
+
+  dune build bin/main.exe \
+  && (pkill -f '^/app/bin/current-bench-pipeline' || echo 'Not running?') \
+  && sleep 0.1 \
+  && cp _build/default/bin/main.exe /app/bin/current-bench-pipeline \
+  && run
+done
