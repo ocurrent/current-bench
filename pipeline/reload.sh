27fcd372287c820ce1ce5f28d50a6df363daf94a
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-12-01 20:07:25 +0530

    Use dune build -watch instead of custom inotifywait command
    Let dune build take care of the source files to watch, instead of listing all
    the files manually in our reload.sh script

--- pipeline/reload.sh
+++ pipeline/reload.sh
@@ -2,27 +2,30 @@
 
 COMMAND="${*@Q}"
 
+DIR=_build/default/bin
+EXE="${DIR}/main.exe"
+
 function run {
   cd /app
   sh -c "$COMMAND" &
+  cd /mnt/project
 }
 
 run
 
-cd /mnt/project
 /mnt/project/dev/github-app.sh
 
 while :
 do
-  cd /mnt/project
-
-  inotifywait -q -q -e CLOSE_WRITE \
-    lib/dune lib/*.ml lib/*.mli \
-    bin/dune bin/*.ml bin/*.mli
-
-  dune build bin/main.exe \
+  # NOTE:
+  # 1. inotifywait exits with a failure status code when main.exe gets deleted
+  #    before a rebuild. So, we watch the directory instead, and make sure the
+  #    file exists.
+  # 2. Also, we sleep for a short while, to ensure the process has been killed
+  #    before copying the new executable and trying to run it.
+  inotifywait -q -e CLOSE_WRITE "${DIR}" | grep -q main.exe \
   && (pkill -f '^/app/bin/current-bench-pipeline' || echo 'Not running?') \
   && sleep 0.1 \
-  && cp _build/default/bin/main.exe /app/bin/current-bench-pipeline \
+  && cp "${EXE}" /app/bin/current-bench-pipeline \
   && run
 done
