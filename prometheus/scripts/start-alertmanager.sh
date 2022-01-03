d5e2f80159383933b69d3d068242d3f9a3b33e95
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-03 13:29:56 +0530

    Add basic configuration for prometheus and alertmanager

---
+++ prometheus/scripts/start-alertmanager.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Script to start alertmanager.
+set -eu
+
+# Alertmanager doesn't provide a way to replace config variables, and we need
+# to write our own script to do this. See
+# https://github.com/prometheus/alertmanager/issues/504
+
+sed "s|\$ALERTMANAGER_SLACK_API_URL|$ALERTMANAGER_SLACK_API_URL|g" /config/alertmanager.yml > /tmp/alertmanager.yml  # Permissions to write to the same file is a problem, so we write a /tmp file
+alertmanager --config.file=/tmp/alertmanager.yml --log.level=debug
