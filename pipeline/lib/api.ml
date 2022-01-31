d517dd609bb2dcf4dfe22d86a90811e046517c06
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-31 16:35:05 +0530

    Allow specifying API token in {development,production}.conf files
    We authenticate using different tokens for different repos

--- pipeline/lib/api.ml
+++ pipeline/lib/api.ml
@@ -1,12 +1,49 @@
 open Lwt.Infix
 module Utils = Cohttp_lwt_unix
 
+let request_token req =
+  let headers = Cohttp.Request.headers req in
+  let token = Cohttp.Header.get headers "Authorization" in
+  let open String in
+  match token with
+  | Some token ->
+      if starts_with ~prefix:"Bearer " token
+      then
+        let n = length token in
+        let m = length "Bearer " in
+        sub token m (n - m)
+      else Fmt.failwith "Invalid token"
+  | _ -> ""
+
+let authenticate_token req api_tokens =
+  (* This function only that the token is a valid token, and doesn't process
+     the body of the request. Essentially, we only do "authentication" of the
+     token. The authorization is done in a separate function.*)
+  (match api_tokens with
+  | [] -> failwith "Server configuration error: API tokens not configured."
+  | _ -> ());
+
+  let token = request_token req in
+  match token with
+  | "" -> failwith "Invalid token"
+  | _ ->
+      List.find_opt
+        (fun (t : Config.api_token) -> String.equal t.token token)
+        api_tokens
+
+let authorize_token (token : Config.api_token option) repository =
+  let repo_id = Repository.info repository in
+  match token with
+  | Some token ->
+      if String.equal token.repo repo_id then () else failwith "Invalid token"
+  | _ -> failwith "Invalid token"
+
 let check_key key json =
   match Yojson.Safe.Util.(member key json) with
   | `Null -> Fmt.failwith "Key '%s' is missing" key
   | s -> s
 
-let make_benchmark_from_request ~conninfo ~body =
+let make_benchmark_from_request ~conninfo ~body ~token =
   let json_body = body |> Yojson.Safe.from_string in
   let open Yojson.Safe.Util in
   let repository_name = check_key "repo_name" json_body |> to_string in
@@ -48,6 +85,7 @@ let make_benchmark_from_request ~conninfo ~body =
     Models.Benchmark.make ~duration ~run_at ~repository ~worker ~docker_image
       ?build_job_id ?run_job_id
   in
+  authorize_token token repository;
   let serial_id =
     Storage.setup_metadata ~repository ~conninfo ~worker ~docker_image
   in
@@ -58,15 +96,16 @@ let make_benchmark_from_request ~conninfo ~body =
              [ Current_bench_json.of_json bench ]));
   Storage.record_success ~conninfo ~serial_id
 
-let capture_metrics conninfo =
+let capture_metrics conninfo api_tokens =
   object
     inherit Current_web.Resource.t
 
-    method! post_raw _ _ body =
+    method! post_raw _ req body =
       Cohttp_lwt.Body.to_string body >>= fun body ->
       let status, response =
         try
-          make_benchmark_from_request ~conninfo ~body;
+          let token = authenticate_token req api_tokens in
+          make_benchmark_from_request ~conninfo ~body ~token;
           let status = `OK in
           let response = `Assoc [ ("success", `Bool true) ] in
           (status, response)
