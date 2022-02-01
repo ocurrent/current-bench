e0471413f623a7baa6cf682ac31de16c29e6c822
ArthurW <arthur@tarides.com>
2022-02-01 18:11:17 +0100

    api: add mock tests for postgresql

--- pipeline/tests/api_test.ml
+++ pipeline/tests/api_test.ml
@@ -1,6 +1,6 @@
 let example_conf =
   [
-    { Config.repo = "myowner/repo"; token = "p455word!" };
+    { Config.repo = "myowner/myrepo"; token = "p455word!" };
     { Config.repo = "other/fst"; token = "token-should-be-unique" };
     { Config.repo = "other/snd"; token = "token-should-be-unique" };
   ]
@@ -16,7 +16,7 @@ let cohttp_unauth_request =
   Cohttp.Request.make uri
 
 let server_not_configured =
-  Alcotest.test_case "server not configured" `Quick @@ fun () ->
+  Alcotest_lwt.test_case_sync "server not configured" `Quick @@ fun () ->
   let req = cohttp_request ~bearer:"Bearer 123" in
   try
     let _ = Api.authenticate_token req [] in
@@ -32,7 +32,7 @@ let missing_bearer =
   with Api.Missing_token -> ()
 
 let invalid_bearer =
-  Alcotest.test_case "invalid bearer" `Quick @@ fun () ->
+  Alcotest_lwt.test_case_sync "invalid bearer" `Quick @@ fun () ->
   let req = cohttp_request ~bearer:"Missing Bearer Prefix" in
   try
     let _ = Api.authenticate_token req example_conf in
@@ -40,7 +40,7 @@ let invalid_bearer =
   with Api.Invalid_token -> ()
 
 let wrong_token =
-  Alcotest.test_case "wrong token" `Quick @@ fun () ->
+  Alcotest_lwt.test_case_sync "wrong token" `Quick @@ fun () ->
   let req = cohttp_request ~bearer:"Bearer wrong-token" in
   match Api.authenticate_token req example_conf with
   | None -> ()
@@ -50,20 +50,90 @@ let wrong_token =
            repo.Config.repo)
 
 let find_repository =
-  Alcotest.test_case "find repository" `Quick @@ fun () ->
+  Alcotest_lwt.test_case_sync "find repository" `Quick @@ fun () ->
   let req = cohttp_request ~bearer:"Bearer p455word!" in
   match Api.authenticate_token req example_conf with
   | None -> Alcotest.fail "expected a repository to be found"
   | Some repo ->
       Alcotest.(check string) "token" repo.Config.token "p455word!";
-      Alcotest.(check string) "token" repo.Config.repo "myowner/repo"
+      Alcotest.(check string) "repo" repo.Config.repo "myowner/myrepo"
 
 let find_other_repository =
-  Alcotest.test_case "find other repository" `Quick @@ fun () ->
+  Alcotest_lwt.test_case_sync "find other repository" `Quick @@ fun () ->
   let req = cohttp_request ~bearer:"Bearer token-should-be-unique" in
   match Api.authenticate_token req example_conf with
   | None -> Alcotest.fail "expected a repository to be found"
-  | Some repo -> Alcotest.(check string) "token" repo.Config.repo "other/fst"
+  | Some repo -> Alcotest.(check string) "repo" repo.Config.repo "other/fst"
+
+open Lwt.Syntax
+
+let ocurrent_site = Current_web.Site.v ~has_role:(fun _ _ -> false) []
+
+let invalid_json =
+  Alcotest_lwt.test_case "invalid json" `Quick @@ fun _ () ->
+  let conninfo = Postgresql.Mock.unused_conninfo in
+  let req = cohttp_request ~bearer:"Bearer token-should-be-unique" in
+  let body = Cohttp_lwt.Body.of_string "this is not json" in
+  let handler = Api.capture_metrics conninfo example_conf in
+  let* resp, body = handler#post_raw ocurrent_site req body in
+  let* body = Cohttp_lwt.Body.to_string body in
+  Alcotest.(check string)
+    "body" body
+    {|{"success":false,"error":"Line 1, bytes 0-16:\nInvalid token 'this is not json'"}|};
+  assert (Cohttp.Response.status resp = `Bad_request);
+  Lwt.return_unit
+
+let empty_benchmarks =
+  Alcotest_lwt.test_case "empty benchmarks" `Quick @@ fun _ () ->
+  let expected_sql =
+    [
+      Postgresql.Expect
+        (fun ?expect query ->
+          let expected =
+            "INSERT INTO benchmark_metadata (run_at, repo_id, commit, branch, \
+             pull_number, pr_title, worker, docker_image) VALUES \
+             (to_timestamp(XXX), 'myowner/myrepo', 'abcd12345', 'main', NULL, \
+             NULL, 'remote', 'external') ON CONFLICT(repo_id, commit, worker, \
+             docker_image) DO UPDATE SET build_job_id = NULL, run_job_id = \
+             NULL, failed = false, cancelled = false, success = false, reason \
+             = '' RETURNING id;"
+          in
+          Alcotest.(check string) "setup metadata" expected query;
+          assert (expect = None);
+          [| [| "421" |] |]);
+      Postgresql.Expect_finish;
+      Postgresql.Expect
+        (fun ?expect query ->
+          let expected =
+            "UPDATE benchmark_metadata SET success = true WHERE id = 421"
+          in
+          Alcotest.(check string) "success" expected query;
+          assert (expect = Some [ Postgresql.Command_ok ]);
+          [||]);
+      Postgresql.Expect_finish;
+    ]
+  in
+  Postgresql.Mock.with_mock expected_sql @@ fun ~conninfo ->
+  let req = cohttp_request ~bearer:"Bearer p455word!" in
+  let input_json =
+    `Assoc
+      [
+        ("repo_owner", `String "myowner");
+        ("repo_name", `String "myrepo");
+        ("branch", `String "main");
+        ("commit", `String "abcd12345");
+        ("run_at", `String "2021-02-03 10:11:12Z");
+        ("benchmarks", `List []);
+      ]
+  in
+  let body = Yojson.Safe.to_string input_json in
+  let body = Cohttp_lwt.Body.of_string body in
+  let handler = Api.capture_metrics conninfo example_conf in
+  let* resp, body = handler#post_raw ocurrent_site req body in
+  let* body = Cohttp_lwt.Body.to_string body in
+  Alcotest.(check string) "body" body {|{"success":true}|};
+  assert (Cohttp.Response.status resp = `OK);
+  Lwt.return_unit
 
 let tests =
   [
@@ -73,4 +143,6 @@ let tests =
     wrong_token;
     find_repository;
     find_other_repository;
+    invalid_json;
+    empty_benchmarks;
   ]
--- pipeline/tests/test.ml
+++ pipeline/tests/test.ml
@@ -1,3 +1,4 @@
 let () =
-  Alcotest.run "pipeline"
-    [ ("api", Api_test.tests); ("json_stream", Json_stream_test.tests) ]
+  Lwt_main.run
+  @@ Alcotest_lwt.run "pipeline"
+       [ ("api", Api_test.tests); ("json_stream", Json_stream_test.tests) ]
