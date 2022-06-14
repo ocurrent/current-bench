ocurrent/current-bench

commit 2ff8bb142205b15fcf8e43262515bd70cca5e303

pipeline: Don't mark stale PRs as closed PRs
d624b236e3695c399aa6cc5ec366874e87673db2 added functionality to filter out
stale PRs (no activity in 2 weeks) when running the benchmarks on all the
repositories that we are watching. This is to prevent running too many
benchmarks when adding a new repository or when restarting current-bench.

But, e467ef945487ba42c6351cb1da213b5fbf3242dc used the filtered out list of
repositories when trying to figure out the PRs which are open, which is a
bug. This commit fixes that bug.

Closes #361

pipeline/lib/pipeline.ml
@@ -167,7 +167,6 @@ let github_repositories repo =
        Refs.refs api repo
   in
   let default_branch = Github.Api.default_ref refs in
-  let stale_timestamp = Util.stale_timestamp () in
   let default_branch_name = Util.get_branch_name default_branch in
   let ref_map = Github.Api.all_refs refs in
   let title_map = refs_with_title in
@@ -183,17 +182,26 @@ let github_repositories repo =
       let repository =
         repository ~commit ~github_head:head ~commit_message:message ?title
       in
-      (* If commit is more than two weeks old, then skip it.*)
-      if Github.Api.Commit.committed_date head > stale_timestamp
-      then
-        match key with
-        (* Skip all branches other than the default branch, and check PRs *)
-        | `Ref branch when branch = default_branch ->
-            repository ~branch:default_branch_name () :: lst
-        | `Ref _ -> lst
-        | `PR pull_number -> repository ~pull_number () :: lst
-      else lst)
+      match key with
+      (* Skip all branches other than the default branch, and check PRs *)
+      | `Ref branch when branch = default_branch ->
+          repository ~branch:default_branch_name () :: lst
+      | `Ref _ -> lst
+      | `PR pull_number -> repository ~pull_number () :: lst)
     ref_map []
+
+let filter_stale_repositories repos =
+  let stale_timestamp = Util.stale_timestamp () in
+  List.filter_map
+    (fun repo ->
+      let head = Repository.github_head repo in
+      (* If commit is more than two weeks old, then skip it.*)
+      match head with
+      | Some head when Github.Api.Commit.committed_date head > stale_timestamp
+        ->
+          Some repo
+      | _ -> None)
+    repos
 
 let repositories = function
   | Source.Local path ->
@@ -248,7 +256,11 @@ let pull_requests_from_repositories repos =
 
 let process_pipeline ~config ~ocluster ~conninfo ~sources () =
   let repos = repositories sources in
+  let fresh_repos = Current.map filter_stale_repositories repos in
   let+ () =
+    let+ open_pulls = Current.map pull_requests_from_repositories repos in
+    Storage.mark_closed_pull_requests ~open_pulls ~conninfo
+  and+ () =
     Config.slack_log ~config ~key:"*repositories outcome*"
     @@ Current.map (fun () -> "ALL GOOD")
     @@ Current.list_iter ~collapse_key:"pipeline"
@@ -256,13 +268,10 @@ let process_pipeline ~config ~ocluster ~conninfo ~sources () =
          (fun repo ->
            let* repo in
            pipeline ~config ~ocluster ~conninfo repo)
-         repos
+         fresh_repos
   and+ () =
     Config.slack_log ~config ~key:"*repositories*"
-      (Current.map string_of_repositories repos)
-  and+ () =
-    let+ open_pulls = Current.map pull_requests_from_repositories repos in
-    Storage.mark_closed_pull_requests ~open_pulls ~conninfo
+      (Current.map string_of_repositories fresh_repos)
   in
   ()
 

commit 3ccd65a432ecefe6a8efcf2024392cbcc62419a2

pipeline: Don't filter out local repository as stale repo
This commit fixes the bug introduced in
2ff8bb142205b15fcf8e43262515bd70cca5e303 which incorrectly filters out
"local/local" repository when filtering stale repositories.

pipeline/lib/pipeline.ml
@@ -200,6 +200,7 @@ let filter_stale_repositories repos =
       | Some head when Github.Api.Commit.committed_date head > stale_timestamp
         ->
           Some repo
+      | None when Repository.info repo = "local/local" -> Some repo
       | _ -> None)
     repos
 