15073ebffbc101386a5a9ab9e96fafef052ace7d
Ambre Austen Suhamy <ambre@tarides.com>
2023-03-21 17:36:07 +0100

    Moving to latest dune

--- pipeline/reload.sh
+++ pipeline/reload.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
+set -eu
 
-COMMAND="${*@Q}"
+cd /mnt/project
 
-DIR=_build/default/bin
-EXE="${DIR}/main.exe"
+./dev/github-app.sh
 
-function run {
-  cd /app
-  sh -c "$COMMAND" &
-  cd /mnt/project
-}
-
-run
-
-dune build --watch bin/main.exe &
-
-/mnt/project/dev/github-app.sh
-
-while :
-do
-  # NOTE:
-  # 1. inotifywait exits with a failure status code when main.exe gets deleted
-  #    before a rebuild. So, we watch the directory instead, and make sure the
-  #    file exists.
-  # 2. Also, we sleep for a short while, to ensure the process has been killed
-  #    before copying the new executable and trying to run it.
-  inotifywait -q -e CLOSE_WRITE "${DIR}" | grep -q main.exe \
-  && (pkill -f '^/app/bin/current-bench-pipeline' || echo 'Not running?') \
-  && sleep 0.1 \
-  && cp "${EXE}" /app/bin/current-bench-pipeline \
-  && run
-done
+dune exec --watch bin/main.exe -- "$@"
