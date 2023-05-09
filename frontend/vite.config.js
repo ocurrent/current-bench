ca034d6544a009b8f9681000dadf4bd3a72b7e75
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-05-09 16:58:31 +0530

    Use process.env instead of import.meta to allow using Jest
    Jest fails with a `SyntaxError: Cannot use 'import.meta' outside a
    module`. This change is a workaround. See:
    https://github.com/vitejs/vite/issues/1149

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), env({ prefix: "VITE_" })],
   server: {
-    port: 8082,
-  },
+    port: 8082
+  }
 });
