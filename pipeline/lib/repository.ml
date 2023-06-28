c676a633e12403cab38343ddaa25987b73495f72
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-06-28 16:27:50 +0530

    Include worker and docker_image query params in GitHub status URL
    The GitHub status URLs didn't include worker and docker image. When a
    repository had multiple configurations or old configurations which were
    removed, the default links sometimes may not open the correct page in the app,
    which could confuse the users. This commits ensures that the worker and docker
    image are included in the URLs to take the users to the correct page from the
    PR checks links.
    
    Closes #453

--- pipeline/lib/repository.ml
+++ pipeline/lib/repository.ml
@@ -70,11 +70,6 @@ let to_path t =
 
 let frontend_url () = Sys.getenv "OCAML_BENCH_FRONTEND_URL"
 
-(* $server/$repo_owner/$repo_name/pull/$pull_number/base/$pull_base *)
-let commit_status_url repo =
-  let uri_end = to_path repo in
-  Uri.of_string (frontend_url () ^ "/" ^ uri_end)
-
 let compare a b =
   let cmp =
     Stdlib.compare
